52f9b9f2933fcf6e3723ffa3614f23703fc25481
bbfsdev <bbfsdev@gmail.com>
2025-09-29 01:47:48 +0000

    Fix sources sequence name.

---
+++ backend/migrations/006_2025_09_29_sources_sequence_to_source.py
@@ -0,0 +1,14 @@
+"""
+Rename sequence from sources_id_seq to source_id_seq
+and update the default value for the id column in the sources table.
+"""
+
+def migrate(migrator, database, fake=False, **kwargs):
+    # Using execute_sql for ALTER SEQUENCE and ALTER TABLE operations
+    database.execute_sql("ALTER SEQUENCE sources_id_seq RENAME TO source_id_seq;")
+    database.execute_sql("ALTER TABLE sources ALTER COLUMN id SET DEFAULT nextval('source_id_seq'::regclass);")
+
+def rollback(migrator, database, fake=False, **kwargs):
+    # Revert the changes made in the migrate function
+    database.execute_sql("ALTER SEQUENCE source_id_seq RENAME TO sources_id_seq;")
+    database.execute_sql("ALTER TABLE sources ALTER COLUMN id SET DEFAULT nextval('sources_id_seq'::regclass);")
